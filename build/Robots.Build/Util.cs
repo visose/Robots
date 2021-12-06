7e74ca7b2a2c756354bce1dc42580587689b55f8
visose <vicente@visose.com>
2021-12-06 11:11:15 +0000

    Fix linux issues

--- build/Robots.Build/Util.cs
+++ build/Robots.Build/Util.cs
@@ -1,40 +1,60 @@
 ﻿using System.Diagnostics;
+using System.Text.Json;
 
 namespace Robots.Build;
 
 static class Util
 {
-    public static string ArtifactsFolder => "Artifacts";
+    public static string ArtifactsFolder => "artifacts";
     public static string BuildFolder => Path.Combine(ArtifactsFolder, "bin", "Robots.Grasshopper", "net48");
     public static string PackageFolder => Path.Combine(ArtifactsFolder, "package");
 
+    public static void Log(string? text)
+    {
+        Console.WriteLine(text);
+    }
+
     public static int Run(string file, string args, string? setCurrentDir = null)
     {
-        var currentDir = Directory.GetCurrentDirectory();
-
-        if (setCurrentDir is not null)
-            Directory.SetCurrentDirectory(setCurrentDir);
+        var currentDir = setCurrentDir ?? Directory.GetCurrentDirectory();
 
         var startInfo = new ProcessStartInfo(file, args)
         {
-            CreateNoWindow = true,
+            FileName = file,
+            Arguments = args,
+            WorkingDirectory = currentDir,
+            RedirectStandardInput = true,
             RedirectStandardOutput = true,
-            RedirectStandardError = true
+            RedirectStandardError = true,
+            UseShellExecute = false,
+            CreateNoWindow = true
         };
 
         using var process = new Process
         {
             StartInfo = startInfo
         };
-
-        process.OutputDataReceived += (o, e) => Console.WriteLine(e.Data);
-        process.ErrorDataReceived += (o, e) => Console.WriteLine(e.Data);
-        process.Start();
+        
+        process.OutputDataReceived += (o, e) => Log(e.Data);
+        process.ErrorDataReceived += (o, e) => Log(e.Data);
+        process.Start();        
         process.BeginErrorReadLine();
         process.BeginOutputReadLine();
         process.WaitForExit();
 
-        Directory.SetCurrentDirectory(currentDir);
         return process.ExitCode;
+    }
+
+    public static string GetSecret(string key)
+    {
+        string? value = Environment.GetEnvironmentVariable(key);
+
+        if (value is not null)
+            return value;
+
+        var json = File.ReadAllText("build/Robots.Build/secrets.json");
+        var doc = JsonDocument.Parse(json);
+        var prop = doc.RootElement.GetProperty(key);
+        return prop.GetString() ?? throw new NullReferenceException($"Secret {key} not found.");
     }
 }
