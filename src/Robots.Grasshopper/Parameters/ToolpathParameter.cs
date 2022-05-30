f14299c19d22583f66ec314b4af4c513427ad9fd
visose <vicente@visose.com>
2022-05-30 23:27:07 +0100

    Clean up icons

--- src/Robots.Grasshopper/Parameters/ToolpathParameter.cs
+++ src/Robots.Grasshopper/Parameters/ToolpathParameter.cs
@@ -2,9 +2,9 @@
 
 public class ToolpathParameter : GH_PersistentParam<GH_Toolpath>
 {
-    public ToolpathParameter() : base("Toolpath", "Toolpath", "This is a robot toolpath", "Robots", "Parameters") { }
+    public ToolpathParameter() : base("Toolpath parameter", "Toolpath", "This is a robot toolpath", "Robots", "Parameters") { }
     public override GH_Exposure Exposure => GH_Exposure.secondary;
-    protected override System.Drawing.Bitmap Icon => Util.GetIcon("iconProgramParam");
+    protected override System.Drawing.Bitmap Icon => Util.GetIcon("iconToolpathParam");
     public override Guid ComponentGuid => new("{715AEDCE-14E8-400B-A226-9806FC3CB7B3}");
     protected override GH_Toolpath PreferredCast(object data) =>
         data is IToolpath cast ? new GH_Toolpath(cast) : null!;
